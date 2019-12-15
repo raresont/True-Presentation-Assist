9d34f3c5c73400870bc8f6ebb34eaec997866fed
Rares Ont <raresont@gmail.com>
2019-12-15 14:44:38 +0200

    add input types

--- src/app/pages/schedule/schedule.ts
+++ src/app/pages/schedule/schedule.ts
@@ -5,6 +5,11 @@ import { AlertController, IonList, LoadingController, ModalController, ToastCont
 import { ConferenceData } from '../../providers/conference-data';
 import { UserData } from '../../providers/user-data';
 
+import { Platform } from '@ionic/angular';
+import { File } from '@ionic-native/file/ngx';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { FileTransfer, FileTransferObject  } from '@ionic-native/file-transfer/ngx';
+
 @Component({
   selector: 'page-schedule',
   templateUrl: 'schedule.html',
@@ -23,6 +28,12 @@ export class SchedulePage implements OnInit {
   groups: any = [];
   confDate: string;
 
+  filePath: string;
+  fileName: string;
+  audio: MediaObject;
+
+  lastPlayed: string = '';
+
   constructor(
     public alertCtrl: AlertController,
     public confData: ConferenceData,
@@ -31,7 +42,11 @@ export class SchedulePage implements OnInit {
     public router: Router,
     public toastCtrl: ToastController,
     public user: UserData,
-    public config: Config
+    public config: Config,
+    private file: File,
+     public platform: Platform,
+    private media: Media
+    , private transfer: FileTransfer
   ) { }
 
   ngOnInit() {
@@ -49,6 +64,47 @@ export class SchedulePage implements OnInit {
     this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).subscribe((data: any) => {
       this.shownSessions = data.shownSessions;
       this.groups = data.groups;
+    });
+  }
+
+  playAudio(file,idx) {
+    if (this.platform.is('ios')) {
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
+      this.audio = this.media.create(this.filePath);
+    } else if (this.platform.is('android')) {
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
+      this.audio = this.media.create(this.filePath);
+    }
+    this.lastPlayed = file;
+    this.audio.play();
+    this.audio.setVolume(0.8);
+  }
+  
+  public requestTranslation() {
+    let fileName= this.lastPlayed;
+    var url = "http://localhost/interpret";
+    if (this.platform.is('ios')) {
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + fileName;
+      this.audio = this.media.create(this.filePath);
+    } else if (this.platform.is('android')) {
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + fileName;
+      this.audio = this.media.create(this.filePath);
+    }
+
+    var options = {
+      fileKey: "file",
+      fileName: fileName,
+      chunkedMode: false,
+      mimeType: "multipart/form-data",
+      params : {'fileName': fileName}
+    };
+   
+    const fileTransfer: FileTransferObject = this.transfer.create();
+
+    fileTransfer.upload(this.filePath, url, options).then(data => {
+      console.log("uploaded")
+    }, err => {
+      console.log("uploaded")
     });
   }
 
